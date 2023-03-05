3e6fd2208c0c4c1544d8f56d1f3a0225aec8d70f
Shunsuke-Hori <hori0518@gmail.com>
2023-03-05 13:28:10 -0800

    add English version webpage

---
+++ docs/js/script.js
@@ -0,0 +1,73 @@
+// Show where the user is on the page by highlighting the corresponding link in the navigation menu
+window.addEventListener('DOMContentLoaded', () => {
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+      } else {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    });
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
+  });
+
+});
+
+// Scroll to a heading when the user clicks on a link, taking into account the height of the header
+document.addEventListener('DOMContentLoaded', function () {
+  var anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+  for (var i = 0; i < anchorLinks.length; i++) {
+    anchorLinks[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var hash = this.hash;
+      var target = document.querySelector(hash);
+      var header = document.querySelector('h1'); /* Replace 'header' with the selector for your header */
+      var headerHeight = header.offsetHeight;
+
+      if (target) {
+        var scrollTop = window.scrollY || document.documentElement.scrollTop;
+        var targetTop = target.getBoundingClientRect().top + scrollTop - headerHeight;
+
+        window.scrollTo({
+          top: targetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  }
+});
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.padding = "2px 10px";
+    document.getElementById("logo").style.fontSize = "20px";
+  } else {
+    document.getElementById("header").style.padding = "15px 10px";
+    document.getElementById("logo").style.fontSize = "20px";
+  }
+}
+
+// When the user clicks on the button with an href to #top, scroll to the top of the document
+document.addEventListener('DOMContentLoaded', function() {
+  var topLink = document.querySelector('a[href="#top"]');
+
+  topLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
